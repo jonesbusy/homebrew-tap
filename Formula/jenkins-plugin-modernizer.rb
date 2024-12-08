6220505cb1a02c67ec8a0ba70f400139b899b205
Valentin Delaye <jonesbusy@gmail.com>
2024-12-08 04:58:37 +0100

    Remove version from url

--- Formula/jenkins-plugin-modernizer.rb
+++ Formula/jenkins-plugin-modernizer.rb
@@ -2,7 +2,7 @@ class JenkinsPluginModernizer < Formula
     desc "Jenkins Plugin Modernizer"
     version "499.vb_86f97f0b_197"
     homepage "https://github.com/jenkins-infra/plugin-modernizer-tool"
-    url "https://repo.jenkins-ci.org/artifactory/releases/io/jenkins/plugin-modernizer/plugin-modernizer-cli/#{version}/plugin-modernizer-cli-#{version}.jar"
+    url "https://repo.jenkins-ci.org/artifactory/releases/io/jenkins/plugin-modernizer/plugin-modernizer-cli/499.vb_86f97f0b_197/plugin-modernizer-cli-499.vb_86f97f0b_197.jar"
     sha256 "0827c8eaf17b7e3221c11b866f5851ce0cab6c35225d4ff38af22713680f845d"
     license "MIT"
     
