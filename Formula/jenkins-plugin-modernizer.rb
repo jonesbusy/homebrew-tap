25d31ebae8774506e8fce5de2529515fd95a8ef1
Valentin Delaye <jonesbusy@gmail.com>
2024-12-08 04:55:09 +0100

    Add version

--- Formula/jenkins-plugin-modernizer.rb
+++ Formula/jenkins-plugin-modernizer.rb
@@ -1,13 +1,14 @@
 class JenkinsPluginModernizer < Formula
     desc "Jenkins Plugin Modernizer"
-    homepage "https://github.com/aragozin/jvm-tools"
-    url "https://repo.jenkins-ci.org/artifactory/releases/io/jenkins/plugin-modernizer/plugin-modernizer-cli/499.vb_86f97f0b_197/plugin-modernizer-cli-499.vb_86f97f0b_197.jar"
+    version "499.vb_86f97f0b_197"
+    homepage "https://github.com/jenkins-infra/plugin-modernizer-tool"
+    url "https://repo.jenkins-ci.org/artifactory/releases/io/jenkins/plugin-modernizer/plugin-modernizer-cli/#{version}/plugin-modernizer-cli-#{version}.jar"
     sha256 "0827c8eaf17b7e3221c11b866f5851ce0cab6c35225d4ff38af22713680f845d"
     license "MIT"
     
     def install
-      libexec.install "plugin-modernizer-cli-499.vb_86f97f0b_197.jar"
-      bin.write_jar_script libexec/"plugin-modernizer-cli-499.vb_86f97f0b_197.jar", "jenkins-plugin-modernizer"
+      libexec.install "plugin-modernizer-cli-#{version}.jar"
+      bin.write_jar_script libexec/"plugin-modernizer-cli-#{version}.jar", "jenkins-plugin-modernizer"
       #(bin/"jenkins-plugin-modernizer").write_env_script libexec/"bin/jenkins-plugin-modernizer", Language::Java.overridable_java_home_env
     end
   
