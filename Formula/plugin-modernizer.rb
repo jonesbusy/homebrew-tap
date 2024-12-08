b76d8772e91fb0ce6945729a6618ddd1420f7a52
jreleaserbot <jreleaser@kordamp.org>
2024-12-08 06:01:04 +0100

    plugin-modernizer 881.vdde52f4467b_7

--- Formula/plugin-modernizer.rb
+++ Formula/plugin-modernizer.rb
@@ -1,16 +1,16 @@
 class PluginModernizer < Formula
     desc "Plugin Modernizer"
-    version "449"
+    version "881.vdde52f4467b_7"
     homepage "https://github.com/jenkins-infra/plugin-modernizer-tool"
-    url "https://repo.jenkins-ci.org/artifactory/releases/io/jenkins/plugin-modernizer/plugin-modernizer-cli/499.vb_86f97f0b_197/plugin-modernizer-cli-499.vb_86f97f0b_197.jar"
-    sha256 "0827c8eaf17b7e3221c11b866f5851ce0cab6c35225d4ff38af22713680f845d"
+    url "https://repo.jenkins-ci.org/artifactory/releases/io/jenkins/plugin-modernizer/plugin-modernizer-cli/881.vdde52f4467b_7/plugin-modernizer-cli-881.vdde52f4467b_7.jar"
+    sha256 "b8a7040d2afa506b4d870c94211583218a6064699d9450de012f774623345304"
     license "MIT"
-    
+
     def install
-      libexec.install "plugin-modernizer-cli-499.vb_86f97f0b_197.jar"
-      bin.write_jar_script libexec/"plugin-modernizer-cli-499.vb_86f97f0b_197.jar", "plugin-modernizer"
+      libexec.install "plugin-modernizer-cli-881.vdde52f4467b_7.jar"
+      bin.write_jar_script libexec/"plugin-modernizer-cli-881.vdde52f4467b_7.jar", "plugin-modernizer"
     end
-  
+
     test do
       system bin/"plugin-modernizer", "--version"
     end
