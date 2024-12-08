1895aa09a51635f513b23ac4187a6c979baade8f
Valentin Delaye <jonesbusy@gmail.com>
2024-12-08 05:04:48 +0100

    Version

--- Formula/jenkins-plugin-modernizer.rb
+++ Formula/jenkins-plugin-modernizer.rb
@@ -1,5 +1,6 @@
 class JenkinsPluginModernizer < Formula
     desc "Jenkins Plugin Modernizer"
+    version "449"
     homepage "https://github.com/jenkins-infra/plugin-modernizer-tool"
     url "https://repo.jenkins-ci.org/artifactory/releases/io/jenkins/plugin-modernizer/plugin-modernizer-cli/499.vb_86f97f0b_197/plugin-modernizer-cli-499.vb_86f97f0b_197.jar"
     sha256 "0827c8eaf17b7e3221c11b866f5851ce0cab6c35225d4ff38af22713680f845d"
