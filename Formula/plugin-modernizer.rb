88c1aeb3593305827d8442e42be7331c6137a7a2
Valentin Delaye <jonesbusy@gmail.com>
2024-12-08 06:02:24 +0100

    Update diff

--- Formula/plugin-modernizer.rb
+++ Formula/plugin-modernizer.rb
@@ -5,12 +5,12 @@ class PluginModernizer < Formula
     url "https://repo.jenkins-ci.org/artifactory/releases/io/jenkins/plugin-modernizer/plugin-modernizer-cli/499.vb_86f97f0b_197/plugin-modernizer-cli-499.vb_86f97f0b_197.jar"
     sha256 "0827c8eaf17b7e3221c11b866f5851ce0cab6c35225d4ff38af22713680f845d"
     license "MIT"
-    
+
     def install
       libexec.install "plugin-modernizer-cli-499.vb_86f97f0b_197.jar"
       bin.write_jar_script libexec/"plugin-modernizer-cli-499.vb_86f97f0b_197.jar", "plugin-modernizer"
     end
-  
+
     test do
       system bin/"plugin-modernizer", "--version"
     end
