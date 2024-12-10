2478387212e5bf21c01ce8a808ad042ea46f5e6d
Valentin Delaye <valentin.delaye@elca.ch>
2024-12-10 12:59:25 +0100

    Update version and URL

--- Formula/plugin-modernizer.rb
+++ Formula/plugin-modernizer.rb
@@ -1,14 +1,14 @@
 class PluginModernizer < Formula
     desc "Plugin Modernizer"
-    version "881.vfb_92e5678fcd".split(".")[0]
+    version "911.v211a_1d5a_7a_a_f".split(".")[0]
     homepage "https://github.com/jenkins-infra/plugin-modernizer-tool"
-    url "https://repo.jenkins-ci.org/artifactory/releases/io/jenkins/plugin-modernizer/plugin-modernizer-cli/881.vfb_92e5678fcd/plugin-modernizer-cli-881.vfb_92e5678fcd.jar"
-    sha256 "e3d5a430f65d00dc53424d5f957da151c55b77794f5fe5369420ce24436edc47"
+    url "https://github.com/jenkins-infra/plugin-modernizer-tool/releases/download/911.v211a_1d5a_7a_a_f/jenkins-plugin-modernizer-911.v211a_1d5a_7a_a_f.jar"
+    sha256 "4aa574dd4c071037be73e36c2b2cd0bf8f2f6c58734cee3d6dd9c94dae8c770c"
     license "MIT"
 
     def install
-      libexec.install "plugin-modernizer-cli-881.vfb_92e5678fcd.jar"
-      bin.write_jar_script libexec/"plugin-modernizer-cli-881.vfb_92e5678fcd.jar", "plugin-modernizer"
+      libexec.install "jenkins-plugin-modernizer-911.v211a_1d5a_7a_a_f.jar"
+      bin.write_jar_script libexec/"jenkins-plugin-modernizer-911.v211a_1d5a_7a_a_f.jar", "plugin-modernizer"
     end
 
     test do
